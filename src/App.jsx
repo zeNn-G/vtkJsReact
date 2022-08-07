c851f82c90bc143e475822fe96b1a01ff5e9f91c
zeNn-G <treroin@gmail.com>
2022-08-08 00:29:57 +0300

    XML

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import React from "react";
 import Start from "./components/Start";
 import Poly from "./components/Poly";
 import ZipHttpReader from "./components/ZipHttpReader";
+import XMLPolyDataWriter from "./components/XMLPolyDataWriterComp";
 
 function App() {
   return (
     <div>
       {/* <Start /> */}
       {/* <Poly /> */}
-      <ZipHttpReader />
+      {/* <ZipHttpReader /> */}
+      <XMLPolyDataWriter />
     </div>
   );
 }
---
+++ src/components/XMLPolyDataWriterComp.jsx
@@ -0,0 +1,89 @@
+import React, { useRef, useState, useEffect } from "react";
+
+import "@kitware/vtk.js/Rendering/Profiles/Geometry";
+
+import vtkFullScreenRenderWindow from "@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow";
+import vtkActor from "@kitware/vtk.js/Rendering/Core/Actor";
+import vtkMapper from "@kitware/vtk.js/Rendering/Core/Mapper";
+
+import vtkHttpDataSetReader from "@kitware/vtk.js/IO/Core/HttpDataSetReader";
+import vtkXMLPolyDataReader from "@kitware/vtk.js/IO/XML/XMLPolyDataReader";
+import vtkXMLPolyDataWriter from "@kitware/vtk.js/IO/XML/XMLPolyDataWriter";
+import vtkXMLWriter from "@kitware/vtk.js/IO/XML/XMLWriter";
+
+import "@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper";
+
+const XMLPolyDataWriter = () => {
+  const vtkContainerRef = useRef(null);
+  const context = useRef(null);
+
+  useEffect(() => {
+    if (context.current) {
+      const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+        background: [0, 0, 0],
+      });
+      const renderer = fullScreenRenderer.getRenderer();
+      const renderWindow = fullScreenRenderer.getRenderWindow();
+
+      const reader = vtkHttpDataSetReader.newInstance({ fetchGzip: true });
+
+      const writer = vtkXMLPolyDataWriter.newInstance();
+      writer.setFormat(vtkXMLWriter.FormatTypes.BINARY);
+      writer.setInputConnection(reader.getOutputPort());
+
+      const writerReader = vtkXMLPolyDataReader.newInstance();
+
+      reader
+        .setUrl(`https://kitware.github.io/vtk-js/data/cow.vtp`, {
+          loadData: true,
+        })
+        .then(() => {
+          const fileContents = writer.write(reader.getOutputData());
+
+          // Try to read it back.
+          const textEncoder = new TextEncoder();
+          writerReader.parseAsArrayBuffer(textEncoder.encode(fileContents));
+          renderer.resetCamera();
+          renderWindow.render();
+
+          const blob = new Blob([fileContents], { type: "text/plain" });
+          const a = window.document.createElement("a");
+          a.href = window.URL.createObjectURL(blob, { type: "text/plain" });
+          a.download = "cow.vtp";
+          a.text = "Download";
+          a.style.position = "absolute";
+          a.style.left = "50%";
+          a.style.bottom = "10px";
+          document.body.appendChild(a);
+          a.style.background = "white";
+          a.style.padding = "5px";
+        });
+
+      const actor = vtkActor.newInstance();
+      const mapper = vtkMapper.newInstance();
+      actor.setMapper(mapper);
+
+      mapper.setInputConnection(writerReader.getOutputPort());
+
+      renderer.addActor(actor);
+
+      context.current = {
+        renderer,
+        renderWindow,
+        reader,
+        writer,
+        writerReader,
+        actor,
+        mapper,
+      };
+    }
+  }, [vtkContainerRef]);
+
+  return (
+    <div>
+      <div ref={vtkContainerRef} />
+    </div>
+  );
+};
+
+export default XMLPolyDataWriter;
