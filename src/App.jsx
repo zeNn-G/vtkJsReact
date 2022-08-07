b827175a10c8db5c6861a137a5d06238757cc0b6
zeNn-G <treroin@gmail.com>
2022-08-07 23:31:27 +0300

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import Start from "./components/Start";
 import Poly from "./components/Poly";
-import VolumeClip from "./components/VolumeClip";
+import ZipHttpReader from "./components/ZipHttpReader";
 
 function App() {
   return (
     <div>
       {/* <Start /> */}
       {/* <Poly /> */}
-      <VolumeClip />
+      <ZipHttpReader />
     </div>
   );
 }
